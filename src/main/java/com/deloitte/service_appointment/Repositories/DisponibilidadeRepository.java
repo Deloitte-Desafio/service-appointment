3d71b889dd103e8c9f2c8aabcd23873867c00a08
Diego <arrudadiego1@gmail.com>
2025-05-31 16:12:31 -0300

    Criação de Repository de Disponibilidade

---
+++ src/main/java/com/deloitte/service_appointment/Repositories/DisponibilidadeRepository.java
@@ -0,0 +1,17 @@
+package com.deloitte.service_appointment.Repositories;
+
+import com.deloitte.service_appointment.Entities.Disponibilidade;
+import com.deloitte.service_appointment.Entities.User;
+import com.deloitte.service_appointment.enums.DiaDaSemana;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DisponibilidadeRepository extends JpaRepository<Disponibilidade, Long> {
+
+    List<Disponibilidade> findByProfissional(User profissional);
+
+    List<Disponibilidade> findByProfissionalAndDiaDaSemana(User profissional, DiaDaSemana diaDaSemana);
+}
