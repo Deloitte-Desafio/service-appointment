580132dad1ce73cb16e879d29dabbbeebe5be6fb
Diego <arrudadiego1@gmail.com>
2025-05-31 13:38:13 -0300

    Criação do Controller de Servico

---
+++ src/main/java/com/deloitte/service_appointment/Controllers/ServiceController.java
@@ -0,0 +1,50 @@
+package com.deloitte.service_appointment.Controllers;
+
+import com.deloitte.service_appointment.DTOs.ServicoRequestDTO;
+import com.deloitte.service_appointment.DTOs.ServicoResponseDTO;
+import com.deloitte.service_appointment.Services.ServicoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/servicos")
+public class ServiceController {
+
+    @Autowired
+    private ServicoService servicoService;
+
+    @GetMapping
+    public ResponseEntity<List<ServicoResponseDTO>> findAll() {
+        List<ServicoResponseDTO> servicos = servicoService.findAll();
+        return ResponseEntity.ok(servicos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ServicoResponseDTO> findById(@PathVariable Long id) {
+        ServicoResponseDTO servicoResponseDTO = servicoService.findById(id);
+        return ResponseEntity.ok(servicoResponseDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ServicoResponseDTO> create(@RequestBody @Valid ServicoRequestDTO servicoRequestDTO) {
+        ServicoResponseDTO servicoResponseDTO = servicoService.create(servicoRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(servicoResponseDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ServicoResponseDTO> update(@PathVariable Long id, @RequestBody @Valid ServicoRequestDTO servicoRequestDTO) {
+        ServicoResponseDTO servicoResponseDTO = servicoService.update(id, servicoRequestDTO);
+        return ResponseEntity.ok(servicoResponseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        servicoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/deloitte/service_appointment/Services/ServicoService.java
+++ src/main/java/com/deloitte/service_appointment/Services/ServicoService.java
@@ -2,9 +2,6 @@ package com.deloitte.service_appointment.Services;
 
 import com.deloitte.service_appointment.DTOs.ServicoRequestDTO;
 import com.deloitte.service_appointment.DTOs.ServicoResponseDTO;
-import com.deloitte.service_appointment.Entities.Servico;
-
-import java.lang.reflect.Type;
 
 public interface ServicoService extends CrudService<Long, ServicoResponseDTO, ServicoRequestDTO> {
 }
